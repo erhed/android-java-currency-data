2172e648a6bd9aa03e048a7f56862e7f593eea15
erhed <erik.hedrenius@gmail.com>
2019-05-28 19:41:11 +0200

    first info card layout

--- app/src/main/java/se/maj7/fx/CurrencyPairInfoActivity.java
+++ app/src/main/java/se/maj7/fx/CurrencyPairInfoActivity.java
@@ -3,11 +3,11 @@ package se.maj7.fx;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CurrencyPairDetailActivity extends AppCompatActivity {
+public class CurrencyPairInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_currency_pair_detail);
+        setContentView(R.layout.activity_currency_pair_info);
     }
 }
